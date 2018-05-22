97d5614a1adf6d09a1853ddcf993e0de25a3c0ba
Ben Elliston <ben.elliston@anu.edu.au>
2018-05-22 15:44:15 +1000

    * mas/db: Use subshells to avoid clunky pushd/popd.

--- mas/db/shard_exists.sh
+++ mas/db/shard_exists.sh
@@ -4,14 +4,8 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 shard=$1
 
-pushd $here
-
-runuser postgres -c 'psql -v ON_ERROR_STOP=1 -A -t -q -d nci' <<EOD
+(cd "$here" &&
+ runuser postgres -c 'psql -v ON_ERROR_STOP=1 -A -t -q -d nci' <<EOD
   select true from ${shard}.paths limit 1
 EOD
-
-rc=$?
-
-popd
-
-exit $rc
+)
