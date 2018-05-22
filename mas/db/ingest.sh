fc9c5c431c1b27307cf2f3e428c86308221a369f
Ben Elliston <ben.elliston@anu.edu.au>
2018-05-22 15:55:37 +1000

    * mas/db: More tweaks.

--- mas/db/ingest.sh
+++ mas/db/ingest.sh
@@ -2,5 +2,5 @@
 
 shard=$1
 
-iconv -f 'ISO-8859-1' -t 'UTF-8' - | sort | uniq | psql -v ON_ERROR_STOP=1 -A -t -q -d nci \
-  -c "set search_path to ${shard}; copy ingest from stdin with (format 'csv', delimiter E'\t', quote E'\b');" >/dev/null
+iconv -f ISO-8859-1 -t UTF-8 - | sort | uniq | psql -v ON_ERROR_STOP=1 -A -t -q -d nci \
+  -c "set search_path to ${shard}; copy ingest from stdin with (format 'csv', delimiter E'\\t', quote E'\\b');" >/dev/null
--- mas/db/shard_exists.sh
+++ mas/db/shard_exists.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 shard=$1
 
-(cd "$here" &&
- runuser postgres -c 'psql -v ON_ERROR_STOP=1 -A -t -q -d nci' <<EOD
+(cd "$here" && runuser postgres -c 'psql -v ON_ERROR_STOP=1 -A -t -q -d nci' <<EOD
   select true from ${shard}.paths limit 1
 EOD
 )
