d6f70d704838a75aff2d1ea9308f016025c741f5
Ben Elliston <ben.elliston@anu.edu.au>
2018-06-05 15:37:55 +1000

    Remove commented out code.

--- grpc_server/gdalservice/pool.go
+++ grpc_server/gdalservice/pool.go
@@ -12,7 +12,6 @@ type ProcessPool struct {
 	Pool      []*Process
 	TaskQueue chan *Task
 	ErrorMsg  chan *ErrorMsg
-	//Health    chan *HealthMsg
 }
 
 func (p *ProcessPool) AddQueue(task *Task) {
@@ -47,52 +46,8 @@ func CreateProcessPool(n int, debug bool) *ProcessPool {
 		p.AddProcess(debug)
 	}
 
-	/*
-		signals := make(chan os.Signal, 1)
-		signal.Notify(signals, syscall.SIGHUP, syscall.SIGINT)
-		go func() {
-			for {
-				select {
-				case <-signals:
-					p.DeleteProcessPool()
-					time.Sleep(1 * time.Second)
-					os.Exit(1)
-				}
-			}
-		}()
-	*/
-
-	/*
-		go func() {
-			for {
-				select {
-				case hMsg := <-p.Health:
-					p.RemoveProcess(hMsg.Address)
-					if hMsg.Replace == true {
-						p.AddProcess(p.Error, p.Health)
-					}
-				}
-			}
-		}()
-	*/
-
 	return p
 }
-
-/*
-func (p *ProcessPool) RemoveProcess(address string) {
-	newPool := []*Process{}
-	for _, proc := range p.Pool {
-		if proc.WarpAddress != address {
-			newPool = append(newPool, proc)
-		}
-		if proc.DrillAddress != address {
-			newPool = append(newPool, proc)
-		}
-	}
-	p.Pool = newPool
-}
-*/
 
 func (p *ProcessPool) DeleteProcessPool() {
 	for _, proc := range p.Pool {
