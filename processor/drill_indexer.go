11abbf6467019ee179d25f15f091634e20831c19
Edison Guo <edison.guo@anu.edu.au>
2018-07-06 15:39:21 +1000

    consolidated mas queries into one to reduce mas_intersects() overhead in mas

--- processor/drill_indexer.go
+++ processor/drill_indexer.go
@@ -47,44 +47,44 @@ func (p *DrillIndexer) Run() {
 	defer close(p.Out)
 	for geoReq := range p.In {
 		var feat geo.Feature
+
 		err := json.Unmarshal([]byte(geoReq.Geometry), &feat)
 		if err != nil {
 			p.Error <- fmt.Errorf("Problem unmarshalling GeoJSON object: %v", geoReq.Geometry)
 			return
 		}
 
+		namespaces := strings.Join(geoReq.NameSpaces, ",")
 		start := time.Now()
-		for _, nameSpace := range geoReq.NameSpaces {
-			reqURL := strings.Replace(fmt.Sprintf("http://%s%s?intersects&metadata=gdal&time=%s&until=%s&srs=%s&namespace=%s&identitytol=%f&dptol=%f", p.APIAddress, geoReq.Collection, geoReq.StartTime.Format(ISOFormat), geoReq.EndTime.Format(ISOFormat), geoReq.CRS, nameSpace, p.IdentityTol, p.DpTol), " ", "%20", -1)
-			featWKT := feat.Geometry.MarshalWKT()
-			resp, err := http.PostForm(reqURL, url.Values{"wkt": {featWKT}})
-			if err != nil {
-				p.Error <- fmt.Errorf("POST request to %s failed. Error: %v", reqURL, err)
-				continue
-			}
+		reqURL := strings.Replace(fmt.Sprintf("http://%s%s?intersects&metadata=gdal&time=%s&until=%s&srs=%s&namespace=%s&identitytol=%f&dptol=%f", p.APIAddress, geoReq.Collection, geoReq.StartTime.Format(ISOFormat), geoReq.EndTime.Format(ISOFormat), geoReq.CRS, namespaces, p.IdentityTol, p.DpTol), " ", "%20", -1)
+		featWKT := feat.Geometry.MarshalWKT()
+		resp, err := http.PostForm(reqURL, url.Values{"wkt": {featWKT}})
+		if err != nil {
+			p.Error <- fmt.Errorf("POST request to %s failed. Error: %v", reqURL, err)
+			continue
+		}
 
-			defer resp.Body.Close()
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				p.Error <- fmt.Errorf("Error parsing response body from %s. Error: %v", reqURL, err)
-				continue
-			}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			p.Error <- fmt.Errorf("Error parsing response body from %s. Error: %v", reqURL, err)
+			continue
+		}
 
-			var metadata MetadataResponse
-			err = json.Unmarshal(body, &metadata)
-			if err != nil {
-				fmt.Println(string(body))
-				p.Error <- fmt.Errorf("Problem parsing JSON response from %s. Error: %v", reqURL, err)
-				continue
-			}
+		var metadata MetadataResponse
+		err = json.Unmarshal(body, &metadata)
+		if err != nil {
+			fmt.Println(string(body))
+			p.Error <- fmt.Errorf("Problem parsing JSON response from %s. Error: %v", reqURL, err)
+			continue
+		}
 
-			switch len(metadata.GDALDatasets) {
-			case 0:
-				p.Out <- &GeoDrillGranule{"NULL", nameSpace, "Byte", nil, geoReq.Geometry, geoReq.CRS}
-			default:
-				for _, ds := range metadata.GDALDatasets {
-					p.Out <- &GeoDrillGranule{ds.DSName, nameSpace, ds.ArrayType, ds.TimeStamps, geoReq.Geometry, geoReq.CRS}
-				}
+		switch len(metadata.GDALDatasets) {
+		case 0:
+			p.Out <- &GeoDrillGranule{"NULL", "EmptyTile", "Byte", nil, geoReq.Geometry, geoReq.CRS}
+		default:
+			for _, ds := range metadata.GDALDatasets {
+				p.Out <- &GeoDrillGranule{ds.DSName, ds.NameSpace, ds.ArrayType, ds.TimeStamps, geoReq.Geometry, geoReq.CRS}
 			}
 		}
 		log.Println("Indexer Time Total", time.Since(start))
