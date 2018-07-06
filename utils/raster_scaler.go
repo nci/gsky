d181eea457f343adf8bf6fd8c859984d1ef4b277
edisonguo <gjmouse@gmail.com>
2018-07-06 14:41:42 +1000

    fixed raster scaling bugs (#136)

--- utils/raster_scaler.go
+++ utils/raster_scaler.go
@@ -11,20 +11,33 @@ type ScaleParams struct {
 }
 
 func scale(r Raster, params ScaleParams) (*ByteRaster, error) {
+	scale := float32(params.Scale)
+	if scale <= 0.0 {
+		if params.Clip <= 0.0 {
+			scale = float32(1.0)
+		} else {
+			scale = float32(float32(254.0) / float32(params.Clip))
+		}
+	}
+
 	switch t := r.(type) {
 	case *ByteRaster:
 		noData := uint8(t.NoData)
-		scale := params.Scale
+		offset := uint8(params.Offset)
 		clip := uint8(params.Clip)
 
 		for i, value := range t.Data {
 			if value == noData {
 				t.Data[i] = 0xFF
 			} else {
+				value += offset
 				if value > clip {
 					value = clip
 				}
-				t.Data[i] = uint8(float64(value) * scale)
+				if value < 0 {
+					value = 0
+				}
+				t.Data[i] = uint8(float32(value) * scale)
 			}
 		}
 		return &ByteRaster{t.Data, t.Height, t.Width, t.NoData}, nil
@@ -32,15 +45,20 @@ func scale(r Raster, params ScaleParams) (*ByteRaster, error) {
 	case *Int16Raster:
 		out := &ByteRaster{NoData: t.NoData, Data: make([]uint8, t.Height*t.Width), Width: t.Width, Height: t.Height}
 		noData := int16(t.NoData)
+		offset := int16(params.Offset)
 		clip := int16(params.Clip)
 		for i, value := range t.Data {
 			if value == noData {
 				out.Data[i] = 0xFF
 			} else {
+				value += offset
 				if value > clip {
 					value = clip
 				}
-				out.Data[i] = uint8(float32(value) * 254.0 / float32(clip))
+				if value < 0 {
+					value = 0
+				}
+				out.Data[i] = uint8(float32(value) * scale)
 			}
 		}
 		return out, nil
@@ -48,34 +66,39 @@ func scale(r Raster, params ScaleParams) (*ByteRaster, error) {
 	case *UInt16Raster:
 		out := &ByteRaster{NoData: t.NoData, Data: make([]uint8, t.Height*t.Width), Width: t.Width, Height: t.Height}
 		noData := uint16(t.NoData)
+		offset := uint16(params.Offset)
 		clip := uint16(params.Clip)
 		for i, value := range t.Data {
 			if value == noData {
 				out.Data[i] = 0xFF
 			} else {
+				value += offset
 				if value > clip {
 					value = clip
 				}
-				out.Data[i] = uint8(float32(value) * 254.0 / float32(clip))
+				if value < 0 {
+					value = 0
+				}
+				out.Data[i] = uint8(float32(value) * scale)
 			}
 		}
 		return out, nil
 
 	case *Float32Raster:
 		out := &ByteRaster{NoData: t.NoData, Data: make([]uint8, t.Height*t.Width), Width: t.Width, Height: t.Height}
-
 		noData := float32(t.NoData)
-		scale := float32(params.Scale)
 		offset := float32(params.Offset)
 		clip := float32(params.Clip)
-
 		for i, value := range t.Data {
 			if value == noData {
 				out.Data[i] = 0xFF
 			} else {
-				value += float32(offset)
+				value += offset
 				if value > clip {
 					value = clip
+				}
+				if value < 0.0 {
+					value = 0.0
 				}
 				out.Data[i] = uint8(value * scale)
 			}
---
+++ utils/raster_scaler_test.go
@@ -0,0 +1,151 @@
+package utils
+
+import (
+	"testing"
+)
+
+func assert(t *testing.T, out *ByteRaster, expected *ByteRaster, err error) {
+	if err != nil {
+		t.Errorf("byte raster test failed,  %v", err)
+	}
+	for i, _ := range out.Data {
+		if out.Data[i] != expected.Data[i] {
+			t.Errorf("byte raster test failed, expecting %v, actual %v", expected.Data, out.Data)
+		}
+	}
+}
+
+func testByteRaster(t *testing.T) {
+	inRaster := make([]Raster, 1)
+
+	sp := ScaleParams{Offset: 1, Scale: 1, Clip: 1000}
+
+	inRaster[0] = &ByteRaster{Data: []uint8{uint8(1), uint8(2)}}
+	expOut := &ByteRaster{Data: []uint8{uint8(2), uint8(3)}}
+	out, err := Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &ByteRaster{Data: []uint8{uint8(1), uint8(2)}}
+	sp = ScaleParams{Offset: 0, Scale: 0, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(127), uint8(254)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &ByteRaster{Data: []uint8{uint8(1), uint8(2)}}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 1000}
+	expOut = &ByteRaster{Data: []uint8{uint8(8), uint8(10)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &ByteRaster{Data: []uint8{uint8(1), uint8(2)}}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(4), uint8(4)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+}
+
+func testInt16Raster(t *testing.T) {
+	inRaster := make([]Raster, 1)
+
+	sp := ScaleParams{Offset: 1, Scale: 1, Clip: 1000}
+
+	inRaster[0] = &Int16Raster{Data: []int16{int16(1), int16(2)}, Height: 2, Width: 1}
+	expOut := &ByteRaster{Data: []uint8{uint8(2), uint8(3)}}
+	out, err := Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &Int16Raster{Data: []int16{int16(1), int16(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 0, Scale: 0, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(127), uint8(254)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &Int16Raster{Data: []int16{int16(1), int16(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 1000}
+	expOut = &ByteRaster{Data: []uint8{uint8(8), uint8(10)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &Int16Raster{Data: []int16{int16(1), int16(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(4), uint8(4)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &Int16Raster{Data: []int16{int16(-100), int16(-200)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(0), uint8(0)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+}
+
+func testUInt16Raster(t *testing.T) {
+	inRaster := make([]Raster, 1)
+
+	sp := ScaleParams{Offset: 1, Scale: 1, Clip: 1000}
+
+	inRaster[0] = &UInt16Raster{Data: []uint16{uint16(1), uint16(2)}, Height: 2, Width: 1}
+	expOut := &ByteRaster{Data: []uint8{uint8(2), uint8(3)}}
+	out, err := Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &UInt16Raster{Data: []uint16{uint16(1), uint16(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 0, Scale: 0, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(127), uint8(254)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &UInt16Raster{Data: []uint16{uint16(1), uint16(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 1000}
+	expOut = &ByteRaster{Data: []uint8{uint8(8), uint8(10)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &UInt16Raster{Data: []uint16{uint16(1), uint16(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(4), uint8(4)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+}
+
+func testFloat32Raster(t *testing.T) {
+	inRaster := make([]Raster, 1)
+
+	sp := ScaleParams{Offset: 1, Scale: 1, Clip: 1000}
+
+	inRaster[0] = &Float32Raster{Data: []float32{float32(1), float32(2)}, Height: 2, Width: 1}
+	expOut := &ByteRaster{Data: []uint8{uint8(2), uint8(3)}}
+	out, err := Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &Float32Raster{Data: []float32{float32(1), float32(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 0, Scale: 0, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(127), uint8(254)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &Float32Raster{Data: []float32{float32(1), float32(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 1000}
+	expOut = &ByteRaster{Data: []uint8{uint8(8), uint8(10)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &Float32Raster{Data: []float32{float32(1), float32(2)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(4), uint8(4)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+
+	inRaster[0] = &Float32Raster{Data: []float32{float32(-100), float32(-200)}, Height: 2, Width: 1}
+	sp = ScaleParams{Offset: 3, Scale: 2, Clip: 2}
+	expOut = &ByteRaster{Data: []uint8{uint8(0), uint8(0)}}
+	out, err = Scale(inRaster, sp)
+	assert(t, out[0], expOut, err)
+}
+
+func TestScale(t *testing.T) {
+	testByteRaster(t)
+	testInt16Raster(t)
+	testUInt16Raster(t)
+	testFloat32Raster(t)
+}
