34c395eb27c043a4775c7c1951ca5a9a02fdf9e1
edisonguo <gjmouse@gmail.com>
2018-11-19 12:09:02 +1100

    made data unavailable legend image transparent by default (#240)

--- utils/empty_tile.go
+++ utils/empty_tile.go
@@ -14,27 +14,29 @@ const tSize = 256
 func GetEmptyTile(imageFilename string, height, width int) ([]byte, error) {
 	canvas := image.NewNRGBA(image.Rect(0, 0, width, height))
 
-	infile, err := os.Open(imageFilename)
-	if err != nil {
-		return nil, err
-	}
-	defer infile.Close()
+	if len(imageFilename) > 0 {
+		infile, err := os.Open(imageFilename)
+		if err != nil {
+			return nil, err
+		}
+		defer infile.Close()
 
-	// Decode will figure out what type of image is in the file on its own.
-	// We just have to be sure all the image packages we want are imported.
-	tile, _, err := image.Decode(infile)
-	if err != nil {
-		return nil, err
-	}
+		// Decode will figure out what type of image is in the file on its own.
+		// We just have to be sure all the image packages we want are imported.
+		tile, _, err := image.Decode(infile)
+		if err != nil {
+			return nil, err
+		}
 
-	for x := 0; x < width; x += tSize {
-		for y := 0; y < height; y += tSize {
-			draw.Draw(canvas, image.Rect(x, y, x+tSize, y+tSize), tile, image.ZP, draw.Src)
+		for x := 0; x < width; x += tSize {
+			for y := 0; y < height; y += tSize {
+				draw.Draw(canvas, image.Rect(x, y, x+tSize, y+tSize), tile, image.ZP, draw.Src)
+			}
 		}
 	}
 
 	buf := new(bytes.Buffer)
-	err = png.Encode(buf, canvas)
+	err := png.Encode(buf, canvas)
 	if err != nil {
 		return nil, err
 	}
