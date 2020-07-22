08b7f7fb9b20a8d316de58b08e9c66f383571e55
Edison Guo <edison.guo@anu.edu.au>
2020-07-22 11:40:08 +1000

    added support for concurrent access to MAS during data ingestion

--- mas/db/shard_refresh.sh
+++ mas/db/shard_refresh.sh
@@ -3,6 +3,16 @@
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 shard=$1
 
+function cleanup()
+{
+(cd "$here" && psql -v -A -t -q -d mas <<EOD
+drop schema if exists ${shard}_tmp cascade;
+drop schema if exists ${shard}_old cascade;
+EOD
+)
+}
+trap cleanup EXIT
+
 (cd "$here" && psql -v ON_ERROR_STOP=1 -A -t -q -d mas <<EOD
 
 set role mas;
@@ -14,6 +24,5 @@ select refresh_caches();
 set search_path to public;
 alter schema ${shard} rename to ${shard}_old;
 alter schema ${shard}_tmp rename to ${shard};
-drop schema ${shard}_old cascade;
 EOD
 )
