78756b40fcc758e6e7bdb460f94a13fe2548b583
Edison Guo <edison.guo@anu.edu.au>
2020-07-22 10:48:01 +1000

    added support for concurrent access to MAS during data ingestion

--- mas/db/shard_ingest.sh
+++ mas/db/shard_ingest.sh
@@ -3,6 +3,8 @@
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 shard=$1
 
-(cd "$here" && ./shard_reset.sh "$shard")
+psql -v ON_ERROR_STOP=1 -A -t -q -d mas \
+  -c "select true from ${shard}_tmp.paths limit 1;" \
+  || (cd "$here" && ./shard_reset.sh "$shard")
 
 (cd "$here" && concurrent -v -b 10000 -r 3 ./ingest.sh "${shard}_tmp")
