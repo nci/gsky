b9886207da0f99b4d987e0d5ef307d9c73b48413
Edison Guo <edison.guo@anu.edu.au>
2020-07-22 10:11:02 +1000

    added support for concurrent access to MAS during data ingestion

--- mas/db/shard_reset.sh
+++ mas/db/shard_reset.sh
@@ -10,7 +10,12 @@ drop schema if exists ${shard}_tmp cascade;
 create schema if not exists ${shard}_tmp;
 set search_path to ${shard}_tmp;
 
+grant usage on schema ${shard}_tmp to public;
+alter default privileges for role mas in schema ${shard}_tmp grant select on tables to public;
+
 \\i shard.sql
+
+grant select,insert,update on ${shard}_tmp.timestamps_cache to api;
 
 EOD
 )
