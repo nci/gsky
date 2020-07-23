a96c69dcd8b598f8d608f13820cd1dd032f4f1b7
edisonguo <edison.guo@anu.edu.au>
2020-07-23 10:10:50 +1000

    added concurrent updates on MAS (#454)
    * added support for concurrent access to MAS during data ingestion
    
    * added support for concurrent access to MAS during data ingestion
    
    * added support for concurrent access to MAS during data ingestion
    
    * added support for concurrent access to MAS during data ingestion
    
    * added support for concurrent access to MAS during data ingestion

--- mas/db/shard_refresh.sh
+++ mas/db/shard_refresh.sh
@@ -3,13 +3,26 @@
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 shard=$1
 
+function cleanup()
+{
+(cd "$here" && psql -v -A -t -q -d mas <<EOD
+drop schema if exists ${shard}_tmp cascade;
+drop schema if exists ${shard}_old cascade;
+EOD
+)
+}
+trap cleanup EXIT
+
 (cd "$here" && psql -v ON_ERROR_STOP=1 -A -t -q -d mas <<EOD
 
 set role mas;
-set search_path to ${shard};
+set search_path to ${shard}_tmp;
 
 select refresh_polygons();
 select refresh_caches();
 
+set search_path to public;
+alter schema ${shard} rename to ${shard}_old;
+alter schema ${shard}_tmp rename to ${shard};
 EOD
 )
--- mas/db/shard_reset.sh
+++ mas/db/shard_reset.sh
@@ -6,11 +6,16 @@ shard=$1
 (cd "$here" && psql -v ON_ERROR_STOP=1 -A -t -q -d mas <<EOD
 
 set role mas;
-set search_path to ${shard};
+drop schema if exists ${shard}_tmp cascade;
+create schema if not exists ${shard}_tmp;
+set search_path to ${shard}_tmp;
 
-truncate paths;
-truncate metadata;
-truncate timestamps_cache;
+grant usage on schema ${shard}_tmp to public;
+alter default privileges for role mas in schema ${shard}_tmp grant select on tables to public;
+
+\\i shard.sql
+
+grant select,insert,update on ${shard}_tmp.timestamps_cache to api;
 
 EOD
 )
